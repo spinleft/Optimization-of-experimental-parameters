e47b9daeff83c24666462633e99a60ba88adf254
spinleft <spinleftgit@gmail.com>
2020-11-30 20:39:41 +0800

    添加文件

--- utilities.py
+++ utilities.py
@@ -158,43 +158,9 @@ def get_normal_params_set(min_boundary, max_boundary, base_params, std_dev, para
 
 
 if __name__ == '__main__':
-    # 最速降线
-    # params = np.array([-1.15722878, -0.51218646, 1.07800144, -2.35206841, 0.85351334, -0.1725775, -0.012383])   # 0.064683930185784
-    # params = np.array([1.32179176, -1.21398091, 0.74667593, -2.54193118, 2.35885283, -1.54051139, 0.43814302])  # 0.038674916857825536
-    # params = np.array([-0.70461515, 1.39923573, -0.65270201, -2.51971808, 2.09282611, -1.92157829, 0.68268599])
-    # params = np.array([-0.7670372, 1.53994717, -0.71413151, -2.81321045, 2.22119171, -1.8959186, 0.67939656])
-    # params = np.array([-1.25691566, 1.64684315, -0.79104391, -3., 1.73756398, -1.89891462, 0.78546396])
-    # 数值模拟
-    # params = np.array([1.45977357, 0.67831924, -0.58728008, 0.51803271, 2.7090874, -0.60886192, -1.87649613])
-    # params = np.array([1.53569274, 1.97344643, -1.31129392, 3., 0.85330547, -3., 0.81725298])   # 0.05954534
-    # params = np.array([1.27987736, 2.28957164, -1.38378197, 3.,          1.06099137, -2.97176395, 0.76509959])
-    # params = np.array([0.07569556,  0.68589037, -0.42546083, -4.9431948 ,  2.79715272,  1.25677803, -1.16114458])   # 0.05812652
-    # params = np.array([0.0191221,   1.08035653, -0.66378105, -5. ,         2.92697467,  1.5467491, -1.46792874])
-    params = np.array([-0.38430342, 2.47892757, -1.67234304, -5., 2.375123, 1.01905152, -0.84496258])   # 0.04283276  # 2020-11-29_22-25
-    # params = np.array([-0.89764876, 1.3078746, -0.79381851, -4.24078172, -0.96142077, 4.06165589, -1.89668127])
-    # 根据参数绘图
-    tf = 10
-    sample_rate = 5000
-    t = np.arange(0, tf, 1 / sample_rate)
-    wave = waveform(1, 0, 10, 5000, params)
-    t_sample = t[[0, 2000, 4000, 5000, 6000, 7000, 8000]]
-    wave_sample = wave[[0, 2000, 4000, 5000, 6000, 7000, 8000]]
-    f = interpolate.interp1d(t_sample, wave_sample, kind='quadratic')
-    t_init = t[:4000]
-    wave_init = f(t_init)
-    plt.plot(t, wave)
-    plt.plot(t_init, wave_init)
-    plt.show()
-    # 随机
-    # min_boundary = np.array([-3., -3., -3., -4., -4., -4., -4.])
-    # max_boundary = np.array([3., 3., 3., 4., 4., 4., 4.])
-    # params = get_random_params_set(min_boundary, max_boundary, 1, 4.5, 0, 3, 20)[0]
-    # print(params)
-    # plot_wave(1, 0, 10, 20, params)
-    # x = np.linspace(0, 1, 1000)
-    # y1 = np.power(x, 1/5)
-    # y2 = np.log2((1 + 511 * x)**(1/9))
-    # plt.plot(x, y1, label='y1')
-    # plt.plot(x, y2, label='y2')
-    # plt.legend()
-    # plt.show()
+    # 查看随机波形随机
+    min_boundary = np.array([-3., -3., -3., -4., -4., -4., -4.])
+    max_boundary = np.array([3., 3., 3., 4., 4., 4., 4.])
+    params = get_random_params_set(min_boundary, max_boundary, 1)[0]
+    print(params)
+    plot_wave(1, 0, 1, 1000, params)
