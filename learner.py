f67fc4dc382eee2aae4bab62447c94441e2a1117
spinleft <spinleftgit@gmail.com>
2020-12-05 20:57:48 +0800

    打开循环随机采样和训练次数递增

--- learner.py
+++ learner.py
@@ -197,6 +197,7 @@ class Learner():
             history = self.net.fit(self.history_params_list,
                                 self.history_costs_list,
                                 self.max_epoch)
+            self.max_epoch += 100
             print("last loss = %f"%history.history['loss'][-1])
             print("training epoches = %d" % len(history.epoch))
             # 测量神经网络拟合误差
@@ -204,8 +205,8 @@ class Learner():
             fit_loss = np.average(np.abs(self.history_costs_list - fit_history_costs_set))
             print("fit_loss = %f" %fit_loss)
             # Step2: 产生预测参数
-            # index = (iteration - 1) % len(self.predict_good_params_set_size)
-            index = 0
+            index = (iteration - 1) % len(self.predict_good_params_set_size)
+            # index = 0
             predict_good_params_set = []
             for window_params in self.window_params_set:
                 predict_good_params_set.append(self.get_predict_good_params_set(window_params, self.predict_good_params_set_size[index]))
